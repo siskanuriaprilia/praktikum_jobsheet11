f2529493aeebc7d3d29bb1b9317d1e3b608b8321
siskanuriaprilia <aprilias878@gmail.com>
2023-11-09 17:49:20 +0700

    percobaan 4_modifikasi 2

--- Quiz.java
+++ Quiz.java
@@ -1,32 +1,34 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Quiz {
-
     public static void main(String[] args) {
-
         Random random = new Random();
         Scanner input27 = new Scanner(System.in);
 
-        char menu='y';
-        do 
-        {
+        char menu = 'y';
+        do {
             int number = random.nextInt(10) + 1;
-            boolean succes = false;
-            do
-            {
+            boolean success = false;
+            do {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input27.nextInt();
                 input27.nextLine();
-                succes = (answer == number);
-            }
-            while(!succes);
-            System.out.print("Apakah anda ingin mengulang permainan (Y/y)? ");
-            menu = input27.nextLine(). charAt(0);
-        }
-            while(menu =='y' || menu=='Y');   
-        {
-            System.out.println("Terima kasih telah bermain!");
-        }
+
+                if (answer < number) {
+                    System.out.println("Terlalu kecil!");
+                } else if (answer > number) {
+                    System.out.println("Terlalu besar!");
+                } else {
+                    success = true;
+                }
+
+            } while (!success);
+
+            System.out.println("Selamat anda berhasil menebak angka!");
+            System.out.println("Apakah Anda ingin mengulang permainan (Y/y)?");
+            menu = input27.nextLine().charAt(0);
+
+        } while (menu == 'y' || menu == 'Y');
     }
 }
