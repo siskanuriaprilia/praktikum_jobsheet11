06bcf89a8a145ace991b4a66b1db4081cc58aeab
siskanuriaprilia <aprilias878@gmail.com>
2023-11-09 19:39:35 +0700

    Percobaan 5_modifikasi 3

--- NestedLoop_2341760038.java
+++ NestedLoop_2341760038.java
@@ -21,11 +21,14 @@ public class NestedLoop_2341760038 {
     for (int i = 0; i < temps.length; i++)
     {
         System.out.println("Kota ke-" + (i + 1) + ": ");
-        for (double temp : temps[i]) 
+        double sum = 0;
+        for (int j = 0; j < temps[0].length; j++) 
         {
             System.out.print(temps[i][1] + " ");
+            sum += temps[i][j];
         }
-        System.out.println();
-     }   
-   }
+        double average = sum / temps[0].length;
+        System.out.println("Rata-rata: " + average); 
+    }
+  }
 }
